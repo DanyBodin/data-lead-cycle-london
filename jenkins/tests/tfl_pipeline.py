e2d13878aeac4b7376cac9df52a892652df61ebc
DanyBodin <d.bodin03@gmail.com>
2024-10-11 09:58:25 +0200

    change to train

--- jenkins/tests/tfl_pipeline.py
+++ jenkins/tests/tfl_pipeline.py
@@ -9,24 +9,8 @@ from app.train import load_data
 def sample_dataset():
     bucket_name = "tfl-cycle"
     folder_name = "silver"
-
-    session = boto3.Session(
-    aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"],
-    region_name="eu-west-3"
-)
-
-    s3_client = session.client('s3')
-    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
-    parquet_files = [content['Key'] for content in response.get('Contents', []) if content['Key'].endswith('.parquet')]
-
-    df_list = []
-    for file in parquet_files:
-        file_path = f"s3://{bucket_name}/{file}"
-        df = pd.read_parquet(file_path, engine='pyarrow', storage_options={"key": os.environ["AWS_ACCESS_KEY_ID"], "secret": os.environ["AWS_SECRET_ACCESS_KEY"]})
-        df_list.append(df)
-
-    return df_list[0]
+    df = load_data(bucket_name, folder_name)
+    return df
 
 def test_load_data(sample_dataset):
     assert not sample_dataset.empty, "Dataframe is empty"
