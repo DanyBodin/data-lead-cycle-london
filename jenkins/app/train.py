6287a3dc41deafd6dcccb8bcb93c0f0c130d6522
DanyBodin <d.bodin03@gmail.com>
2024-10-11 10:47:01 +0200

    change acces6

--- jenkins/app/train.py
+++ jenkins/app/train.py
@@ -24,17 +24,7 @@ def load_data(bucket_name, folder_name):
 
     s3_client = session.client('s3')
 
-    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
-
-    parquet_files = [content['Key'] for content in response.get('Contents', []) if content['Key'].endswith('.parquet')]
-
-    df_list = []
-    for file in parquet_files:
-        file_path = f"s3://{bucket_name}/{file}"
-        df = pd.read_parquet(file_path, engine='pyarrow', storage_options={"key": os.environ["AWS_ACCESS_KEY_ID"], "secret": os.environ["AWS_SECRET_ACCESS_KEY"]})
-        df_list.append(df)
-
-    return df_list[0]
+    return s3_client
 
 if __name__ == "__main__":
     experiment_name = "tfl-cycle-assertion"
